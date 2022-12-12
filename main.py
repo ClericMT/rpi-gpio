61d7b5ddf27439d741af70a5e4305ac23eded9a8
ClericMT <43632579+ClericMT@users.noreply.github.com>
2022-12-12 19:28:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO            # import RPi.GPIO module  
 from time import sleep             # lets us have a delay  
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setup(40, GPIO.OUT)           # set GPIO24 as an output   
   
 try:  
     while True:  
